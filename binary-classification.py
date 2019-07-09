1ff0fa6b32fa7b7815567dc0da22eabc7b79f33e
Facundo HM <facundo.hmz@gmail.com>
2019-07-09 18:57:54 -0300

    Upgrade binary-classification to TensorFlow 2

--- binary-classification.py
+++ binary-classification.py
@@ -1,65 +1,55 @@
 import tensorflow as tf
 from tensorflow import keras
 
-word_count = 10000
-pad_value = 0
-sequence_maxlen = 256
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
 
-# Load data
-imdb = keras.datasets.imdb
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(
-    num_words=word_count
+batch_size = 512
+
+# Create subsplit for trainig
+train_subsplit = tfds.Split.TRAIN.subsplit([6, 4])
+
+# Load and split data
+(train_data, validation_data), test_data = tfds.load(
+    name="imdb_reviews",
+    split=(train_subsplit, tfds.Split.TEST),
+    as_supervised=True
 )
 
-# Standardize train_data length
-train_data = keras.preprocessing.sequence.pad_sequences(
-    train_data,
-    value=pad_value,
-    padding='post',
-    maxlen=sequence_maxlen
-)
+url = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
 
-# Standardize test_data length
-test_data = keras.preprocessing.sequence.pad_sequences(
-    test_data,
-    value=pad_value,
-    padding='post',
-    maxlen=sequence_maxlen
+# Create Keras layer
+hub_layer = hub.KerasLayer(
+    url,
+    input_shape=[],
+    dtype=tf.string,
+    trainable=True
 )
 
 # Define model layers
 model = keras.Sequential([
-    keras.layers.Embedding(word_count, 16),
-    keras.layers.GlobalAveragePooling1D(),
-    keras.layers.Dense(16, activation=tf.nn.relu),
-    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+    hub_layer,
+    keras.layers.Dense(16, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid')
 ])
+
+# Print model summary
+model.summary()
 
 # Configure model
 model.compile(
     optimizer='adam',
     loss='binary_crossentropy',
-    metrics=['acc']
+    metrics=['accuracy']
 )
 
-model.summary()
-
-# Create a validation set
-validation_train_data = train_data[:10000]
-partial_train_data = train_data[10000:]
-
-validation_train_labels = train_labels[:10000]
-partial_train_labels = train_labels[10000:]
-
-# Train and monitor model
+# Train model
 model.fit(
-    partial_train_data,
-    partial_train_labels,
-    epochs=20,
-    batch_size=512,
-    validation_data=(validation_train_data, validation_train_labels),
+    train_data.shuffle(10000).batch(batch_size),
+    epochs=10,
+    validation_data=validation_data.batch(batch_size),
     verbose=1
 )
 
 # Evaluate model
-model.evaluate(test_data, test_labels)
+model.evaluate(test_data.batch(batch_size), verbose=1)
