2f2ea4def5868fe14bf0c21a8a3823b9a9271071
Facundo HM <facundo.hmz@gmail.com>
2024-04-09 19:36:14 -0300

    Update requirements file

--- binary_classification.py
+++ binary_classification.py
@@ -1,55 +1,43 @@
 import tensorflow as tf
 from tensorflow import keras
-
 import tensorflow_hub as hub
 import tensorflow_datasets as tfds
 
-batch_size = 512
+BATCH_SIZE = 512
+URL = 'https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1'
 
-# Create subsplit for trainig
 train_subsplit = tfds.Split.TRAIN.subsplit([6, 4])
-
-# Load and split data
 (train_data, validation_data), test_data = tfds.load(
-    name="imdb_reviews",
+    name='imdb_reviews',
     split=(train_subsplit, tfds.Split.TEST),
     as_supervised=True
 )
 
-url = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
-
-# Create Keras layer
 hub_layer = hub.KerasLayer(
-    url,
+    URL,
     input_shape=[],
     dtype=tf.string,
     trainable=True
 )
-
-# Define model layers
 model = keras.Sequential([
     hub_layer,
     keras.layers.Dense(16, activation='relu'),
     keras.layers.Dense(1, activation='sigmoid')
 ])
 
-# Print model summary
 model.summary()
 
-# Configure model
 model.compile(
     optimizer='adam',
     loss='binary_crossentropy',
     metrics=['accuracy']
 )
 
-# Train model
 model.fit(
-    train_data.shuffle(10000).batch(batch_size),
+    train_data.shuffle(10000).batch(BATCH_SIZE),
     epochs=10,
-    validation_data=validation_data.batch(batch_size),
+    validation_data=validation_data.batch(BATCH_SIZE),
     verbose=1
 )
 
-# Evaluate model
-model.evaluate(test_data.batch(batch_size), verbose=1)
+model.evaluate(test_data.batch(BATCH_SIZE), verbose=1)
