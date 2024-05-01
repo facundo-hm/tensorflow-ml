7f4cb2916d068dc4626ea14853b406ab4b172f2a
Facundo HM <facundo.hmz@gmail.com>
2024-05-01 19:23:42 -0300

    Update overfitting underfitting code

--- overfitting_underfitting.py
+++ overfitting_underfitting.py
@@ -1,20 +1,22 @@
-import tensorflow as tf
-from tensorflow import data
 from utils import (
-    utils, optimizers, callbacks, metrics, losses,
+    optimizers, callbacks, metrics, losses,
     Sequential, layers)
 import pandas as pd
 import numpy as np
 
-Load_Response = tuple[data.Dataset, data.Dataset, data.Dataset]
-
+SEQUENTIAL = Sequential
 FEATURES = 28
 N_ROWS = 100000
-N_VALIDATION = int(1e3)
-N_TRAIN = int(1e4)
-BUFFER_SIZE = int(1e4)
+N_VALIDATION = 0.2
 BATCH_SIZE = 500
-STEPS_PER_EPOCH = 160
+STEPS_PER_EPOCH = (N_ROWS - (N_ROWS * N_VALIDATION)) // BATCH_SIZE
+MAX_EPOCHS = 100
+COLUMN_NAMES = ['class_label', 'jet_1_b-tag', 'jet_1_eta', 'jet_1_phi',
+    'jet_1_pt', 'jet_2_b-tag', 'jet_2_eta', 'jet_2_phi', 'jet_2_pt',
+    'jet_3_b-tag', 'jet_3_eta', 'jet_3_phi', 'jet_3_pt', 'jet_4_b-tag',
+    'jet_4_eta', 'jet_4_phi', 'jet_4_pt', 'lepton_eta', 'lepton_pT',
+    'lepton_phi', 'm_bb', 'm_jj', 'm_jjj', 'm_jlv', 'm_lv', 'm_wbb',
+    'm_wwbb', 'missing_energy_magnitude', 'missing_energy_phi']
 
 # gz = utils.get_file(
 #     'HIGGS.csv.gz',
@@ -22,25 +24,25 @@ STEPS_PER_EPOCH = 160
 
 data_train = pd.read_csv(
    '~/.keras/datasets/HIGGS.csv',
-   names=['class_label', 'jet_1_b-tag', 'jet_1_eta', 'jet_1_phi', 'jet_1_pt', 'jet_2_b-tag', 'jet_2_eta', 'jet_2_phi', 'jet_2_pt', 'jet_3_b-tag', 'jet_3_eta', 'jet_3_phi', 'jet_3_pt', 'jet_4_b-tag', 'jet_4_eta', 'jet_4_phi', 'jet_4_pt', 'lepton_eta', 'lepton_pT', 'lepton_phi', 'm_bb', 'm_jj', 'm_jjj', 'm_jlv', 'm_lv', 'm_wbb', 'm_wwbb', 'missing_energy_magnitude', 'missing_energy_phi'],
-   nrows=N_ROWS
-)
+   names=COLUMN_NAMES,
+   nrows=N_ROWS)
 
-data_labels = data_train.pop('class_label')
+data_labels = data_train.pop(COLUMN_NAMES[0])
 
 lr_schedule = optimizers.schedules.InverseTimeDecay(
-  0.001,
-  decay_steps=STEPS_PER_EPOCH * 10,
-  decay_rate=1,
-  staircase=False)
+    0.001,
+    decay_steps=STEPS_PER_EPOCH * 10,
+    decay_rate=1,
+    staircase=False)
 
 normalizer = layers.Normalization(axis=-1)
 normalizer.adapt(np.array(data_train))
 
-def compile_and_fit(model, optimizer=None, max_epochs=100):
-    if optimizer is None:
-        optimizer = optimizers.Adam(lr_schedule)
-
+def compile_and_fit(
+    model: SEQUENTIAL,
+    optimizer=optimizers.Adam(lr_schedule),
+    max_epochs=MAX_EPOCHS
+):
     model.compile(
         optimizer=optimizer,
         loss=losses.BinaryCrossentropy(from_logits=True),
@@ -56,11 +58,10 @@ def compile_and_fit(model, optimizer=None, max_epochs=100):
         data_labels,
         epochs=max_epochs,
         batch_size=BATCH_SIZE,
-        validation_split=0.2,
+        validation_split=N_VALIDATION,
         callbacks=callbacks.EarlyStopping(
             monitor='val_binary_crossentropy', patience=200),
-        verbose=1
-        )
+        verbose=1)
 
     return history
 
