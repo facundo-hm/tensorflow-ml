a11e822ba05f51b0c0fa16cb9cb58624104fcb93
Facundo HM <facundo.hmz@gmail.com>
2024-08-04 23:39:44 -0300

    Update overfitting underfitting code

--- overfitting_underfitting.py
+++ overfitting_underfitting.py
@@ -1,14 +1,15 @@
 from utils import (
-    optimizers, callbacks, metrics, losses,
-    Sequential, layers, regularizers, utils)
+    optimizers, callbacks, metrics, losses, Sequential,
+    layers, regularizers, utils, constraints)
 import pandas as pd
+import numpy as np
 
 FEATURES = 28
 N_ROWS = 100000
 N_VALIDATION = 0.2
 BATCH_SIZE = 500
 STEPS_PER_EPOCH = (N_ROWS - (N_ROWS * N_VALIDATION)) // BATCH_SIZE
-MAX_EPOCHS = 100
+MAX_EPOCHS = 20
 COLUMN_NAMES = ['class_label', 'jet_1_b-tag', 'jet_1_eta', 'jet_1_phi',
     'jet_1_pt', 'jet_2_b-tag', 'jet_2_eta', 'jet_2_phi', 'jet_2_pt',
     'jet_3_b-tag', 'jet_3_eta', 'jet_3_phi', 'jet_3_pt', 'jet_4_b-tag',
@@ -20,12 +21,12 @@ utils.get_file(
     'HIGGS.csv.gz',
     'http://mlphysics.ics.uci.edu/data/higgs/HIGGS.csv.gz')
 
-data_train = pd.read_csv(
+X = pd.read_csv(
     '~/.keras/datasets/HIGGS.csv',
     names=COLUMN_NAMES,
     nrows=N_ROWS)
 
-data_labels = data_train.pop(COLUMN_NAMES[0])
+y = X.pop(COLUMN_NAMES[0])
 
 # Gradually reduce the learning rate during training
 lr_schedule = optimizers.schedules.InverseTimeDecay(
@@ -46,19 +47,23 @@ model = Sequential([
     layers.Input((FEATURES,)),
     layers.Dense(
         512, activation='elu',
-        kernel_regularizer=regularizers.l2(0.0001)),
+        kernel_regularizer=regularizers.l2(0.0001),
+        kernel_constraint=constraints.max_norm(1.)),
     layers.Dropout(0.5),
     layers.Dense(
         512, activation='elu',
-        kernel_regularizer=regularizers.l2(0.0001)),
+        kernel_regularizer=regularizers.l2(0.0001),
+        kernel_constraint=constraints.max_norm(1.)),
     layers.Dropout(0.5),
     layers.Dense(
         512, activation='elu',
-        kernel_regularizer=regularizers.l2(0.0001)),
+        kernel_regularizer=regularizers.l2(0.0001),
+        kernel_constraint=constraints.max_norm(1.)),
     layers.Dropout(0.5),
     layers.Dense(
         512, activation='elu',
-        kernel_regularizer=regularizers.l2(0.0001)),
+        kernel_regularizer=regularizers.l2(0.0001),
+        kernel_constraint=constraints.max_norm(1.)),
     layers.Dropout(0.5),
     layers.Dense(1)
 ])
@@ -74,11 +79,18 @@ model.compile(
 model.summary()
 
 model.fit(
-    data_train,
-    data_labels,
-    epochs=MAX_EPOCHS,
-    batch_size=BATCH_SIZE,
+    X, y, epochs=MAX_EPOCHS, batch_size=BATCH_SIZE,
     validation_split=N_VALIDATION,
     callbacks=callbacks.EarlyStopping(
         monitor='val_binary_crossentropy', patience=200),
     verbose=1)
+
+# Implement Monte Carlo Dropout technique that boosts dropout models
+# and provides better uncertainty estimates.
+# Make 10 predictions over the test set
+y_mcd_probas = np.stack([model(X, training=True) for _ in range(10)])
+# Average over the first dimension
+y_mcd = y_mcd_probas.mean(axis=0)
+
+print('Predict: ', model.predict(X[:1]), y[:1])
+print('Predict MCD: ', y_mcd[:1])
