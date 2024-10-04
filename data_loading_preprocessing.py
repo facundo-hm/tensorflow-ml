167ed7d853ad7fa10cb67fff8422a9f4c1787a8d
Facundo HM <facundo.hmz@gmail.com>
2024-10-04 19:39:26 -0300

    Refactor data loading preprocessing code

--- data_loading_preprocessing.py
+++ data_loading_preprocessing.py
@@ -16,32 +16,37 @@ dataset = train_dataset.interleave(
 
 car_names: list[tf.Tensor] = []
 
-def parse_csv_line(line):
-    defs = [0.] * 2 + [tf.constant('', dtype=tf.string)] + [0.] * 40
-    fields = tf.io.decode_csv(line, record_defaults=defs)
-    car_names.append(fields[2])
-    fields[25], fields[42] = fields[42], fields[25]
+def parse_csv_dataset(dataset: tf.data.Dataset):
+    new_dataset = []
 
-    return fields[1:]
+    for line in dataset.as_numpy_iterator():
+        defs = (
+            [0.] * 2
+            + [tf.constant('', dtype=tf.string)]
+            + [0.] * 40)
+        fields = tf.io.decode_csv(line, record_defaults=defs)
+        car_names.append(fields[2])
+        fields[25], fields[42] = fields[42], fields[25]
+        new_dataset.append(fields[1:])
 
-parsed_dataset = []
+    return new_dataset
 
-for line in dataset.as_numpy_iterator():
-    parsed_line = parse_csv_line(line)
-    parsed_dataset.append(parsed_line)
+parsed_dataset = parse_csv_dataset(dataset)
 
-car_names = tf.strings.strip(car_names)
-le = LabelEncoder()
-car_names_encoded = le.fit_transform(car_names)
+def add_encoded_name(
+    dataset: list[tf.Tensor], car_names: list[tf.Tensor]
+):
+    new_dataset: list[tuple]= []
+    car_names = tf.strings.strip(car_names)
+    le = LabelEncoder()
+    car_names_encoded = le.fit_transform(car_names)
 
-def add_encoded_name(idx, line):
-    line[1] = tf.constant(car_names_encoded[idx], dtype=tf.float32)
-    return tf.stack(line[:-1]), tf.stack(line[-1:])
+    for i, line in enumerate(dataset):
+        line[1] = tf.constant(car_names_encoded[i], dtype=tf.float32)
+        new_dataset.append((tf.stack(line[:-1]), tf.stack(line[-1:])))
+    
+    return new_dataset
 
-new_dataset: list[tuple]= []
+parsed_dataset = add_encoded_name(parsed_dataset, car_names)
 
-for i, line in enumerate(parsed_dataset):
-    X_y = add_encoded_name(i, line)
-    new_dataset.append(X_y)
-
-print(new_dataset)
+print(parsed_dataset)
