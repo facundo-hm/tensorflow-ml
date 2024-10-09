6b6c9a085967a599a61ccb3ffe0e27552b11e87e
Facundo HM <facundo.hmz@gmail.com>
2024-10-08 23:45:33 -0300

    Update data loading preprocessing code

--- data_loading_preprocessing.py
+++ data_loading_preprocessing.py
@@ -1,8 +1,6 @@
 import tensorflow as tf
-from sklearn.preprocessing import LabelEncoder, StandardScaler
 from utils import (
     Sequential, losses, optimizers, layers, activations, metrics)
-import numpy as np
 
 BASE_PATH = './datasets/car_price/'
 SEED = 42
@@ -16,7 +14,6 @@ valid_csv_dataset = tf.data.Dataset.list_files(
 
 def parse_csv_dataset(dataset: tf.data.Dataset):
     new_dataset = []
-    car_names: list[tf.Tensor] = []
 
     for line in dataset.as_numpy_iterator():
         defs = (
@@ -24,23 +21,18 @@ def parse_csv_dataset(dataset: tf.data.Dataset):
             + [tf.constant('', dtype=tf.string)]
             + [0.] * 40)
         fields = tf.io.decode_csv(line, record_defaults=defs)
-        car_names.append(fields[2])
+        # Move Price column at the end
         fields[25], fields[42] = fields[42], fields[25]
-        new_dataset.append(fields[1:])
+        # Remove CarId and CarName columns
+        new_dataset.append(fields[1] + fields[3:])
 
-    return new_dataset, car_names
+    return new_dataset
 
-def add_encoded_name(
-    dataset: list[tf.Tensor], car_names: list[tf.Tensor]
-):
+def add_encoded_name(dataset: list[tf.Tensor]):
     data: list[tf.Tensor]= []
     lables: list[tf.Tensor]= []
-    car_names = tf.strings.strip(car_names)
-    le = LabelEncoder()
-    car_names_encoded = le.fit_transform(car_names)
 
-    for i, line in enumerate(dataset):
-        line[1] = tf.constant(car_names_encoded[i], dtype=tf.float32)
+    for line in dataset:
         data.append(line[:-1])
         lables.append(line[-1:][0])
     
@@ -53,8 +45,8 @@ def create_dataset(
     dataset = dataset.interleave(
         lambda filepath: tf.data.TextLineDataset(filepath),
         cycle_length=n_readers, num_parallel_calls=n_readers)
-    dataset, car_names = parse_csv_dataset(dataset)
-    dataset = add_encoded_name(dataset, car_names).cache()
+    dataset = parse_csv_dataset(dataset)
+    dataset = add_encoded_name(dataset).cache()
     dataset = dataset.shuffle(shuffle_buffer_size, seed=seed)
 
     return dataset.repeat(50).batch(batch_size).prefetch(1)
@@ -69,8 +61,11 @@ y_train = train_dataset.map(lambda x, y: y)
 normalizer = layers.Normalization()
 normalizer.adapt(X_train)
 
+train_dataset = train_dataset.map(lambda X, y: (normalizer(X), y))
+valid_dataset = valid_dataset.map(lambda X, y: (normalizer(X), y))
+test_dataset = test_dataset.map(lambda X, y: (normalizer(X), y))
+
 model = Sequential([
-    normalizer,
     layers.Dense(41, activation=activations.relu,
         kernel_initializer='he_normal'),
     layers.Dense(41, activation=activations.relu,
@@ -83,7 +78,7 @@ model.compile(
     loss=losses.MeanSquaredError(),
     metrics=[metrics.MeanSquaredError()])
 model.fit(train_dataset, validation_data=valid_dataset, epochs=160)
-model.evaluate(test_dataset)
+model.evaluate(test_dataset, verbose=2)
 
 prediction = model.predict(X_train)
 
