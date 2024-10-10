1262c90014fb73cf6a21f757802fe040dbed8648
Facundo HM <facundo.hmz@gmail.com>
2024-10-10 00:01:31 -0300

    Update data loading preprocessing code

--- data_loading_preprocessing.py
+++ data_loading_preprocessing.py
@@ -13,7 +13,8 @@ valid_csv_dataset = tf.data.Dataset.list_files(
     BASE_PATH + 'valid_*.csv', seed=SEED)
 
 def parse_csv_dataset(dataset: tf.data.Dataset):
-    new_dataset = []
+    data: list[tf.Tensor]= []
+    lables: list[tf.Tensor]= []
 
     for line in dataset.as_numpy_iterator():
         defs = (
@@ -21,21 +22,9 @@ def parse_csv_dataset(dataset: tf.data.Dataset):
             + [tf.constant('', dtype=tf.string)]
             + [0.] * 40)
         fields = tf.io.decode_csv(line, record_defaults=defs)
-        # Move Price column at the end
-        fields[25], fields[42] = fields[42], fields[25]
-        # Remove CarId and CarName columns
-        new_dataset.append(fields[1] + fields[3:])
+        data.append(fields[1:2] + fields[3:25] + fields[26:])
+        lables.append(fields[25])
 
-    return new_dataset
-
-def add_encoded_name(dataset: list[tf.Tensor]):
-    data: list[tf.Tensor]= []
-    lables: list[tf.Tensor]= []
-
-    for line in dataset:
-        data.append(line[:-1])
-        lables.append(line[-1:][0])
-    
     return tf.data.Dataset.from_tensor_slices((data, lables))
 
 def create_dataset(
@@ -45,8 +34,7 @@ def create_dataset(
     dataset = dataset.interleave(
         lambda filepath: tf.data.TextLineDataset(filepath),
         cycle_length=n_readers, num_parallel_calls=n_readers)
-    dataset = parse_csv_dataset(dataset)
-    dataset = add_encoded_name(dataset).cache()
+    dataset = parse_csv_dataset(dataset).cache()
     dataset = dataset.shuffle(shuffle_buffer_size, seed=seed)
 
     return dataset.repeat(50).batch(batch_size).prefetch(1)
