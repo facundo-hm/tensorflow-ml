74fa5ba5a5f7390a6b88a6df3d05fc5fe5e2d516
Facundo HM <facundo.hmz@gmail.com>
2024-10-10 23:14:13 -0300

    Refactor data loading preprocessing code

--- data_loading_preprocessing.py
+++ data_loading_preprocessing.py
@@ -12,39 +12,43 @@ test_csv_dataset = tf.data.Dataset.list_files(
 valid_csv_dataset = tf.data.Dataset.list_files(
     BASE_PATH + 'valid_*.csv', seed=SEED)
 
-def parse_csv_dataset(dataset: tf.data.Dataset):
+def decode_line(line: str):
+    defs = (
+        [0.] * 2 + [tf.constant('', dtype=tf.string)] + [0.] * 40)
+    return tf.io.decode_csv(line, record_defaults=defs)
+
+def create_dataset(dataset: tf.data.Dataset, n_readers=3):
+    return dataset.interleave(
+        lambda filepath: tf.data.TextLineDataset(filepath),
+        cycle_length=n_readers, num_parallel_calls=n_readers)
+
+def parse_csv_dataset(
+    dataset: tf.data.Dataset, n_readers=3, seed=SEED,
+    batch_size=25, shuffle_buffer_size=20
+):
+    dataset = create_dataset(dataset, n_readers)
+
     data: list[tf.Tensor]= []
     lables: list[tf.Tensor]= []
 
     for line in dataset.as_numpy_iterator():
-        defs = (
-            [0.] * 2
-            + [tf.constant('', dtype=tf.string)]
-            + [0.] * 40)
-        fields = tf.io.decode_csv(line, record_defaults=defs)
+        fields = decode_line(line)
+        # Remove Car ID, Car Model and Price columns
         data.append(fields[1:2] + fields[3:25] + fields[26:])
+        # Price column
         lables.append(fields[25])
 
-    return tf.data.Dataset.from_tensor_slices((data, lables))
-
-def create_dataset(
-    dataset: tf.data.Dataset, n_readers=3, seed=SEED,
-    batch_size=25, shuffle_buffer_size=20
-):
-    dataset = dataset.interleave(
-        lambda filepath: tf.data.TextLineDataset(filepath),
-        cycle_length=n_readers, num_parallel_calls=n_readers)
-    dataset = parse_csv_dataset(dataset).cache()
+    dataset = tf.data.Dataset.from_tensor_slices(
+        (data, lables)).cache()
     dataset = dataset.shuffle(shuffle_buffer_size, seed=seed)
 
     return dataset.repeat(50).batch(batch_size).prefetch(1)
 
-train_dataset = create_dataset(train_csv_dataset)
-valid_dataset = create_dataset(valid_csv_dataset)
-test_dataset = create_dataset(test_csv_dataset)
+train_dataset = parse_csv_dataset(train_csv_dataset)
+valid_dataset = parse_csv_dataset(valid_csv_dataset)
+test_dataset = parse_csv_dataset(test_csv_dataset)
 
 X_train = train_dataset.map(lambda x, y: x)
-y_train = train_dataset.map(lambda x, y: y)
 
 normalizer = layers.Normalization()
 normalizer.adapt(X_train)
@@ -65,8 +69,18 @@ model.compile(
     optimizer=optimizers.Adam(),
     loss=losses.MeanSquaredError(),
     metrics=[metrics.MeanSquaredError()])
-model.fit(train_dataset, validation_data=valid_dataset, epochs=160)
-model.evaluate(test_dataset, verbose=2)
+# model.fit(train_dataset, validation_data=valid_dataset, epochs=160)
+# model.evaluate(test_dataset, verbose=2)
 
-prediction = model.predict(X_train)
+def extract_categories():
+    ds = train_csv_dataset.concatenate(
+        test_csv_dataset).concatenate(valid_csv_dataset)
+    ds = create_dataset(ds)
 
+    categories: list[tf.Tensor]= [
+        decode_line(line)[2] for line in ds.as_numpy_iterator()]
+
+    return tf.strings.strip(categories)
+
+car_names = extract_categories()
+print('car_names', car_names)
